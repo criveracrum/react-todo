cc4efdd16519b141b8f940d0181fd328d9c424fb
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-09-24 14:05:18 -0500

    Adding functions to CreateToDo

--- src/todo/CreateTodo.js
+++ src/todo/CreateTodo.js
@@ -1,23 +1,40 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default function CreateTodo ({user}) {
 
+    const description = ''
+    const title = ''
+
+    function handleTodoSubmit(evt){
+        
+        if (title){
+            alert('Title required')
+        } else {
+            const today = new Date(Date.now())
+            const newTodo = new Todo(this.title, this.description, today.toDateString())
+            alert('Todo was submitted' + newTodo)
+        }
+    }
+    function onDescriptionChange(evt){
+        this.description = evt.target.value;
+    }
+    function onTitleChange(evt){
+        this.title = evt.target.value;
+    }
 
      return (
-          <form onSubmit={e => e.preventDefault()}>
-             
-             <div>Author: <b>{user}</b></div>
-
+          <form onSubmit={e => e.preventDefault()}>  
              <div>
                  <label htmlFor="create-title">Title:</label>
-                 <input type="text" name="create-title" id="create-title"  required/>
+                 <input type="text" name="create-title" id="create-title" onChange="onTitleChange" required/>
              </div>
              <div>
                  <label htmlFor="create-description">Description:</label>
                  <br />
-                 <textarea />
+                 <textarea onChange="onDescriptionChange"/>
              </div>
-             <input type="submit" value="Create" onChange= ""/>
+             <input type="submit" value="Create" />
          </form>   
           )
  }
