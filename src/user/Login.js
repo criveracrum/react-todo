373904ec42ae7ae9bf5e1f0913e06cf6eb8fa81a
Chadwick Rivera-Crum <criveracrum@gmail.com>
2021-10-19 19:17:23 -0500

    hw4 update for create, toggle, delete, user action

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,4 +1,5 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import { useResource } from 'react-request-hook';
 import { StateContext } from '../Contexts';
 
 export default function Login() {
@@ -8,15 +9,36 @@ export default function Login() {
 
     const [username, setUsername] = useState('');
 
+    const [ loginFailed, setLoginFailed ] = useState(false)
+    const [ password, setPassword ] = useState('')
+
     function handleUsername (evt) { setUsername(evt.target.value) } 
+    function handlePassword (evt) { setPassword(evt.target.value) }
+
+    const [ user, login ] = useResource((username, password) => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            if (user.data.length > 0) {
+                            setLoginFailed(false)
+                            dispatch({ type: 'LOGIN', username: user.data[0].username })
+            } else {
+                            setLoginFailed(true)
+                    }
+            } 
+            }, [user])
 
    return (
-        <form onSubmit={evt => {evt.preventDefault(); dispatch({type:"LOGIN", username})} }>
+        <form onSubmit={evt => {evt.preventDefault(); login(username, password)} }>
             <label htmlFor="login-username">Username:</label>
             <input type="text" name="login-username" value={username} onChange={handleUsername}  id="login-username" />
             <label htmlFor="login-password">Password:</label>
-            <input type="password" name="login-password" id="login-password" />
+            <input type="password" value={password} onChange={handlePassword} name="login-username" id="login-username" />
             <input type="submit" value="Login" />
+            {loginFailed && <span style={{ color: 'red' }}>Invalid username or password</span>}
         </form>
     )
 }
